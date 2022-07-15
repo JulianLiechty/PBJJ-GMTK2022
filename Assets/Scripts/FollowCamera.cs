2fc727cc26f2ab774bd0e4f755763cfef219a026
Grayson Spencer <graysonspencer1212@gmail.com>
2022-07-15 14:47:30 -0600

    Space now causes dice to roll.

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    private GameObject Dice;
+    private GameObject DiceObject;
     [SerializeField]
     private Vector3 cameraOffset;
+    private DiceRoller roller;
 
     // Start is called before the first frame update
     void Start()
     {
-        Dice = GameObject.FindGameObjectsWithTag("Dice")[0];
+        DiceObject = GameObject.FindGameObjectsWithTag("Dice")[0];
+        roller = DiceObject.GetComponent<DiceRoller>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Dice.transform.position;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            roller.ShouldSwing();
+        }
+
+        transform.position = DiceObject.transform.position;
         transform.position = cameraOffset + transform.position;
     }
 }
