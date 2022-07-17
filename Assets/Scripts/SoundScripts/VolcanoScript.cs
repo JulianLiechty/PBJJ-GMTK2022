c50fa68447697547c40253647e18c55b195866c7
Grayson Spencer <graysonspencer1212@gmail.com>
2022-07-17 10:33:31 -0600

    Volcano tests

---
+++ Assets/Scripts/SoundScripts/VolcanoScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoScript : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Dice"))
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached("Event:/AMB/AMB_Volcano", this.gameObject);
+        }
+    }
+}
