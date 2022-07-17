548455a94fc45a3f36607d31dfd811a4dc6e019f
lukereynoldsgames <lukereynoldsgames@gmail.com>
2022-07-17 05:37:39 -0600

    level design push

--- Assets/Trampoline.cs
+++ Assets/Trampoline.cs
@@ -7,12 +7,18 @@ public class Trampoline : MonoBehaviour
     public float xForce;
     public float yForce = 500f;
     public float zForce;
+    public float multiplier = 0.5f;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Dice"))
         {
             collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(xForce, yForce, zForce));
+            Debug.Log("bouncin'");
+        }
+        if (collision.gameObject.CompareTag("BounceOnPad"))
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(xForce, yForce * multiplier, zForce));
             Debug.Log("bouncin'");
         }
     }
